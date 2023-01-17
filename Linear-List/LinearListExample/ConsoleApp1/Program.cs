52aaa5390f6909026844aadf4f2c0570fe5e6c4e
nuna092 <56553943+nuna092@users.noreply.github.com>
2023-01-17 21:24:03 +0300

    Second commit
    Отредактирован файл с программой

---
+++ Linear-List/LinearListExample/ConsoleApp1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace LinearListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NodeList nodeList = new NodeList();
+            nodeList.AddLast("1");
+            nodeList.AddLast("2");
+            nodeList.AddLast("3");
+            nodeList.AddLast("4");
+        }
+    }
+
+    class Node
+    {
+        public object Data {get; set;}
+        public Node NextNode { get; set; }
+    }
+
+    class NodeList
+    {
+        public Node Head {get; set;}
+        public void AddLast(object data)
+        {
+            if(Head == null)
+            {
+                AddHead(data);
+                return;
+            }
+            AddNextNode(data);
+        }
+
+        private void AddHead(object data)
+        {
+            Head = new Node() { Data = data };
+            return;
+        }
+
+        private void AddNextNode(object data)
+        {
+            Node current = Head;
+            while (current.NextNode != null)
+            {
+                current = current.NextNode;
+            }
+            current.NextNode = new Node() { Data = data };
+        }
+    }
+ 
+}
